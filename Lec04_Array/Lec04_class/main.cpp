0686ede659bff59954384fef3ceb500e6d4eb141
sojaehyuk <sirwtn52@naver.com>
2025-04-02 09:34:30 +0900

    25.4.2

--- Lec04_Array/Lec04_class/main.cpp
+++ Lec04_Array/Lec04_class/main.cpp
@@ -1,20 +1,30 @@
 #include "Complex.h"
 #include "Car.h"
+#include "SportsCar.h"
 void main()
 {
-//	Complex a, b, c;
-//	a.read ("A = ");
-//	b.read ("B = ");
-//	c.add (a, b);
-//	a.print ("A = ");
-//	b.print ("B = ");
-//	c.print ("A+B= ");
-
+	/*
+	Complex a, b, c;
+	a.read ("A = ");
+	b.read ("B = ");
+	c.add (a, b);
+	a.print ("A = ");
+	b.print ("B = ");
+	c.print ("A+B= ");
+	*/
 	Car myCar(9, "K3",2);
 	Car momCar(10, "K5", 1);
+	SportsCar mysecCar(99, "911", 2);
 
 	myCar.whereAmI();
 	momCar.whereAmI();
 	myCar.changeGear(3);
 	myCar.display();
+	
+	//4.2¿œ
+
+	SportsCar.
+
+
+	
 }
