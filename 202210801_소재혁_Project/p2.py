31d79d3410cb174c21202106785f5540d17474a4
sojaehyuk <sirwtn52@naver.com>
2025-06-19 19:31:53 +0900

    project 제출

--- 202210801_소재혁_Project/p2.py
+++ 202210801_소재혁_Project/p2.py
@@ -1,21 +1,3 @@
-'''
-# 프로젝트 문제 2번
-input = ")))()"
-
-def problem2(input):
-    # 이 곳에 코드를 작성하세요.
-    # 입력 힌트
-    for char in input:
-        print(char)
-
-    result = 0
-    return result
-
-result = problem2(input)
-
-assert result == 3
-print("정답입니다.")
-'''
 
 
 # 프로젝트 문제 2번
--- 202210801_소재혁_Project/p3.py
+++ 202210801_소재혁_Project/p3.py
@@ -1,41 +1,4 @@
-'''
-# 프로젝트 문제 3번
-input = [[4, 3, 2, 1],
-         [0, 0, 0, 0],
-         [0, 0, 9, 0],
-         [1, 2, 3, 4]]
-N = 4
 
-forest = []
-
-def problem3(input):
-    bear_size = 2
-    honeycomb_count = 0
-    time = 0
-    bear_x, bear_y = 0, 0
-    # 입력 힌트
-
-    # forest 리스트를 input 리스트로 초기화
-    forest = [row[:] for row in input]
-    
-    # 곰의 초기 위치 찾기
-    for i in range(N):
-        for j in range(N):
-            if forest[i][j] == 9:
-                bear_x, bear_y = i, j
-                forest[i][j] = 0
-    print("곰의 초기 위치 x : {0}, y : {1}".format(bear_x, bear_y))
-
-    #여기에서부터 코드를 작성하세요.
-
-    result = 0
-    return result
-
-result = problem3(input)
-
-assert result == 14
-print("정답입니다.")
-'''
 
 # 프로젝트 문제 3번
 input = [[4, 3, 2, 1],
